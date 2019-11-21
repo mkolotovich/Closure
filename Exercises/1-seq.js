ab1333d5f7d7e9fe8ce8d2fff63850d4f2b5ab14
k2miha <k2punk@gmail.com>
2019-11-21 17:54:38 +0300

    Make hw

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,9 @@
 'use strict';
 
-const seq = f => g => x => 0;
+// const seq = f => g => x => {
+//   f = 7; g = 2; const f1 = x => x * g; const f2 = x => f1(x) + f;
+//   return f2(x);
+// };
+const seq = f => g => (typeof(g) === 'function' ? seq(a => f(g(a))) : f(g));
 
 module.exports = { seq };
