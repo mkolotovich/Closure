233fae85f64aab33990ce52bd67c5a334c872008
k2miha <k2punk@gmail.com>
2020-01-09 11:15:36 +0300

    copy alien code

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,23 @@
 'use strict';
 
-const array = () => null;
+// const array = x => ({
+//   function() {
+//     const a = [];
+//     return a;
+//   },
+//   push(a) {
+//     console.log(a.push);
+//     a.push(x);
+//     return this;
+//   }
+// });
+
+const array = () => {
+  const arr = [];
+  const fn = i => arr[i];
+  fn.push = x => arr.push(x);
+  fn.pop = () => arr.pop();
+  return fn;
+};
 
 module.exports = { array };
